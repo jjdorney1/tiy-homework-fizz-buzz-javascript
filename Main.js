90321ff0d175c5919417f5f6d04519e6754086f2
jeffreydorney <jjdorney1@gmail.com>
2016-08-09 16:11:49 -0400

    successfully completed fizzbuzz

--- Main.js
+++ Main.js
@@ -4,3 +4,22 @@
 
 // fizzbuzz javascript program
 
+var start = 1;
+var end = 100;
+var fizz = "Fizz";
+var buzz = "Buzz";
+
+// doIt(start, end);
+
+for(var currentNumber = start; currentNumber <= end; currentNumber++) {
+    if( (currentNumber % 3 == 0) && (currentNumber % 5 == 0) ) {
+        console.log(fizz + buzz);
+    } else if(currentNumber % 3 == 0) {
+        console.log(fizz);
+    } else if(currentNumber % 5 == 0) {
+        console.log(buzz);
+    } else {
+        console.log(currentNumber);
+    }
+}
+
